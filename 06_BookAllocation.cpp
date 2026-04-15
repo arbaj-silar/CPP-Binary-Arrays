f922d5801289172d33dd49be31132d8e2dceba65
Arbaj <silararbaj@gmail.com>
2026-04-16 04:43:36 +0530

    Book Allocation Problem

---
+++ 06_BookAllocation.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool isValid(vector<int> &arr, int n , int m, int maxAllowedPages) {
+    int st = 1, page = 0;
+
+    for(int i=0; i<n; i++) {
+        if(arr[i] > maxAllowedPages) {
+            return false;
+        }
+
+        if(page+arr[i] <= maxAllowedPages) {
+            page += arr[i];
+        } else {
+            st++;
+            page = arr[i];
+        }
+    }
+
+    return st > m ? false: true;
+}
+
+int bookAllocate(vector<int> &arr, int n , int m) {
+    if(m>n) return -1;
+    int sum = 0;
+    for(int val: arr) {
+        sum += val;
+    }
+
+    int st = 0, end = sum;
+    int ans = -1;
+    while(st < end) {
+        int mid = st + (end-st)/2;
+        if(isValid(arr, n, m, mid)) {
+            ans = mid;
+            end = mid-1; 
+        } else {
+            st = mid+1;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> arr = {2, 1, 3, 4};
+    int n = 4, m = 2;
+
+    cout<<"Allocate books: "<<bookAllocate(arr, n, m);
+    cout<<endl;
+    return 0;
+}
